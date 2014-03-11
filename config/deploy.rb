040217e313a826ef9d5e67219ba0bc1e138dcfa0
Alexandre Perez <alexandre.perez92@gmail.com>
2014-03-11 17:05:24 +0100

    Add capistrano and yml app secret config

---
+++ config/deploy.rb
@@ -0,0 +1,63 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'translate_community'
+set :repo_url, 'https://0e9e6f18846ef88655d0c719d444fa4d57c4b616@github.com/thedarkmammouth/translate_community.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/devweb/TranslateCommunity'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :default_env, {
+    'http_proxy' => 'http://proxy.he-arc.ch:8080',
+    'https_proxy' => 'http://proxy.he-arc.ch:8080'
+}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
