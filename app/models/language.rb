af7ac4b642b5636392a42121519800fea32b6f56
pellenberger <pat18.e@gmail.com>
2014-03-15 12:48:25 +0100

    Add many-to-many relation between projects and languages

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,8 @@
 class Language < ActiveRecord::Base
   has_many :user_languages
   has_many :users, through: :user_languages
+  has_many :project_languages
+  has_many :projects, through: :project_languages
 
   validates :code, :name, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :items, dependent: :destroy
+  has_many :project_languages
+  has_many :languages, through: :project_languages
 
   validates :user_id, :name, :description, presence: true
 end
