53f86c3e1bef3484fb057f17d50fff4a4377695a
Alexandre Perez <alexandre.perez92@gmail.com>
2014-04-04 14:16:18 +0200

    Correct sort for translations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ActiveRecord::Base
   belongs_to :project
-  has_many :translations, dependent: :destroy
+  has_many :translations, dependent: :destroy, :order => "created_at DESC"
 
   validates :project_id, :key, presence: true
+
+  def translations_sorted
+    sorted = translations.sort_by { |translation| [translation.score, translation.created_at] }
+    best = sorted.pop
+    sorted.sort_by(&:created_at).reverse.unshift(best)
+  end
 end
