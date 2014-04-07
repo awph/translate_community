18e71dd714b5097f37c047303577fbd85ac4f07d
Alexandre Perez <alexandre.perez92@gmail.com>
2014-04-07 23:15:02 +0200

    fix translations sort, add help on item key

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,15 @@ class Item < ActiveRecord::Base
 
   validates :project_id, :key, presence: true
 
-  def translations_sorted
-    sorted = translations.sort_by { |translation| [translation.score, translation.created_at] }
+  def translations_sorted(language)
+    sorted = translations.select { |translation| translation.language.id == language.id }
+    sorted = sorted.sort_by { |translation| [translation.score, translation.created_at] }
     best = sorted.pop
     sorted = sorted.sort_by(&:created_at).reverse.unshift(best) unless best.nil?
     sorted
+  end
+
+  def first_translation
+    translations.sort_by(&:created_at).reverse.pop
   end
 end
